6b3f13960d6e1cd5b2ff24702faee5cc49a1914d
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-11-05 02:31:18 +0200

    The tasks from fourth lecture.

---
+++ Practice/Lesson 4 bases/Lesson 4 bases/ProductInformation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_bases
+{
+    struct ProductInformation
+    {
+        public string productName;
+        public string receiptDate;
+        public string storageLife;
+        public string provider;
+        public int weight;
+        public int value;
+
+        //Структура не может иметь конструктора по умолчанию (т.е. без параметров), ко всему этому так же надо знать,
+        //что при написании ЛЮБОГО конструктора, необходимо инициализировать ВСЕ поля структуры в его теле.
+        public ProductInformation
+            (string productName, string receiptDate, string storageLife, string provider, int weight, int value)
+        {
+            this.productName = productName;
+            this.provider = provider;
+            this.receiptDate = receiptDate;
+            this.storageLife = storageLife;
+            this.value = value;
+            this.weight = weight;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", productName, receiptDate, storageLife, provider, weight, value);
+        }
+    }
+}
---
+++ Practice/Lesson 4 bases/Lesson 4 bases/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_bases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static string SayHelloWithName(string name)
+        {
+            return String.Format("Hello, {0}", name);
+        }
+
+        static int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int InvertDigits(int number)
+        {
+            int grade = 1;
+            int result = 0;
+
+            if(number < 10)
+            {
+                result = number;
+            }
+            else
+            {
+                while(number > 0)
+                {
+                    result *= grade;
+                    result += number % 10;
+                    number /= 10;
+                    grade = 10;
+                }
+            }
+
+            return result;
+        }
+
+        static bool IsEven(int number)
+        {
+            if(number == 0)
+            {
+                throw new FormatException("Argument must not be zero");
+            }
+            else { }
+
+            //Or, also we can write ((number & 1) != 1)
+            return ((number % 2) == 0);
+        }
+
+        static void TrianglePS(double a, out double P, out double S)
+        {
+            P = 3 * a;
+            S = a * a * Math.Sqrt(3) * 4;
+        }
+
+        static int Sum(params int[] numbers)
+        {
+            int result = 0;
+
+            foreach(int number in numbers)
+            {
+                result += number;
+            }
+
+            return result;
+        }
+
+        static int DigitSum(int number)
+        {
+            int result = 0;
+
+            if(number < 10)
+            {
+                return number;
+            }
+
+            result += number % 10;
+            result += DigitSum(number / 10);
+
+            return result;
+        }
+
+        static double GetMathematic(double a, double b, Mathematic operation)
+        {
+            double result = 0;
+
+            switch(operation)
+            {
+                case Mathematic.add:
+                    {
+                        result = a + b;
+                        break;
+                    }
+                case Mathematic.sub:
+                    {
+                        result = a - b;
+                        break;
+                    }
+                case Mathematic.div:
+                    {
+                        result = a / b;
+                        break;
+                    }
+                case Mathematic.mul:
+                    {
+                        result = a * b;
+                        break;
+                    }
+            }
+
+            return result;
+        }
+    }
+}
